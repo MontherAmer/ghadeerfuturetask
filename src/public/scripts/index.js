2daaaffe00329a559c44b75e31c3db04d33c09b9
montheramer <monther.amer48@gmail.com>
2022-04-16 02:07:50 +0300

    FIX not loading on mobile

--- src/public/scripts/index.js
+++ src/public/scripts/index.js
@@ -5,7 +5,7 @@ const { closeModal } = require('./_modal');
 
 let { initFilters } = require('./_store');
 
-window.onload = async function () {
+window.addEventListener('load', function (){
   fetchLookups();
   initFilters();
   await getListOfJobs();
@@ -17,7 +17,21 @@ window.onload = async function () {
 
   let rightArrow = document.getElementById('times-solid');
   rightArrow.addEventListener('click', () => hideSideBar());
-};
+});
+
+// window.onload = async function () {
+//   fetchLookups();
+//   initFilters();
+//   await getListOfJobs();
+//   let searchInput = document.getElementById('search-input');
+//   searchInput.addEventListener('keydown', (e) => processChange(e));
+
+//   let leftArrow = document.getElementById('bars-solid');
+//   leftArrow.addEventListener('click', () => showSideBar());
+
+//   let rightArrow = document.getElementById('times-solid');
+//   rightArrow.addEventListener('click', () => hideSideBar());
+// };
 
 const reportWindowSize = () => {
   let body = document.getElementsByTagName('body')[0];
